f2cf1e91a76984f03f433f6f3c718c6e41fbdce1
Jhesua Machado <jhesua_machado@hotmail.com>
2026-03-27 10:03:50 -0300

    cale-se

--- ReservationRequest.cpp
+++ ReservationRequest.cpp
@@ -17,10 +17,17 @@ public:
         int start_hour,
         int end_hour,
         int student_count
-    );
+    )
+    {
+        this->course_name = course_name;
+        this->weekday = weekday;
+        this->start_hour = start_hour;
+        this->end_hour = end_hour;
+        this->student_count = student_count;
+    };
     //destrutor
-    ~ReservationRequest();
-
+    ~ReservationRequest() = default;
+        
     int getStartHour();
     int getEndHour();
     std::string getCourseName();
